2822d4c9dc45e1947fbdc725ef00a433571c04a8
Razvan <razfv07@gmail.com>
2024-02-22 12:33:12 +0000

    feat: gazebo implementation of the map

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import setup
 
 package_name = 'f1tenth_gym_agent'
@@ -10,6 +12,9 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob("launch/*.launch.py")),
+        (os.path.join('share', package_name, 'worlds'), glob("worlds/*.world")),
+        (os.path.join('share', package_name, 'urdf'), glob("urdf/*.urdf")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
